0c967145abd0ab186dd2a7d206a2a230fc7642ee
Kum1rdance <nazar200311111@gmail.com>
2021-11-22 00:26:15 +0300

    Basic java

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// 1 Переменные (3)
+//  camelCase
+//const firstName = 'Nazar'
+//let age = 18 //number 
+//const isProgrammer = true //boolean
+
+// 2 Мутирование 
+//('Имя человека: '  + firstName  + '\nВозраст человека: '+ age)
+//const lastName = prompt('Введите фамилию')
+//alert(firstName+' '+lastName)
+// 3 Операторы 
+//let currentYear = 2020
+//const birthYear = 1993
+
+//const age = currentYear - birthYear
+//const a=10
+//const b=5
+
+//console.log(a-b)
+//console.log(currentYear++)
+//console.log(--currentYear)
+
+//let c = 32
+//c = c + a
+//console.log(c)
+// 4 Типы даных 
+//const isProgrammer = true 
+//const name = 'Nazar'
+//const age = 26
+//console.log(typeof isProgrammer)
+//console.log(typeof name)
+//console.log(typeof age)
+// 5 Приоритет операторов 
+//const fullAge = 18
+//const birthYear = 2003
+//const currentYear = 2020
+//const isfullAge = currentYear - birthYear >= fullAge
+//console.log(isfullAge)
+// 6 Условные операторы 
+const courseStatus = 'ready'
+if (courseStatus==='ready'){
+    console.log('Курс уже готов и его можно проходить')
+} else if (courseStatus==='pending'){
+    console.log('Курс находится в процесе разработки')
+} else {
+    console.log('Курс не получ')
+}
+
+
+
+
+
+
+
